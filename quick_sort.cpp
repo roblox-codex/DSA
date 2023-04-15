ec105b91062e2e33e6389db569a87451f7b13675
Digvijay Singh <107340811+roblox-codex@users.noreply.github.com>
2023-04-15 18:27:17 +0530

    quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quickSort(vector<int>& arr, int left, int right) {
+    int i = left, j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j) quickSort(arr, left, j);
+    if (i < right) quickSort(arr, i, right);
+}
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1, 5, 6};
+    int n = arr.size();
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
